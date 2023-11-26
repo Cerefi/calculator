cffa57d8b99f191cec7b6c01133e2f3cf15a1752
Fercie <ferciecaseria.xix@gmail.com>
2023-11-26 21:50:18 +0800

    Fixed a concatonation error in display
    Fixed an error where a number is concatonated if the initial screen.Textcontent is '0'

--- external.js
+++ external.js
@@ -1,6 +1,22 @@
 let firstNumber;
 let secondNumber;
-let operator;
+const screen = document.querySelector('#display');
+const numbers = document.querySelectorAll('.number');
+const operators = document.querySelectorAll('.op')
+let display = '';
+
+numbers.forEach(number => {
+    number.addEventListener('click', ()=>{
+        if (screen.textContent === '0' && number.textContent === '0') {
+            screen.textContent = '0';
+        }
+        else {
+            display += number.textContent;
+            screen.textContent = display;
+        }
+    })
+})
+
 
 function add(a, b) {
     return a + b;
